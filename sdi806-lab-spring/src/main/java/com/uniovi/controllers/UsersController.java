01f1b11cbf23d0a19cdc689bc5fad35dc9b718a9
Luis <UO257809@uniovi.es>
2019-02-23 16:33:56 +0100

    SDI-SpringBoot-Sesión-4.2-Roles

--- sdi806-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi806-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.User;
+import com.uniovi.services.RolesService;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
 import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
+	@Autowired
+	private RolesService rolesService;
 	@Autowired
 	private UsersService usersService;
 
@@ -36,7 +39,7 @@ public class UsersController {
 
 	@RequestMapping(value = "/user/add")
 	public String getUser(Model model) {
-		model.addAttribute("usersList", usersService.getUsers());
+		model.addAttribute("rolesList", rolesService.getRoles());
 		return "user/add";
 	}
 
@@ -102,6 +105,7 @@ public class UsersController {
 		if (result.hasErrors()) {
 			return "signup";
 		}
+		user.setRole(rolesService.getRoles()[0]);
 		usersService.addUser(user);
 		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
 		return "redirect:home";
