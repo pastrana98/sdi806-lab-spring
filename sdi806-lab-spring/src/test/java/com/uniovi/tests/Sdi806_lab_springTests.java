346c5b426535cfda16b658471f4cc04461f36bef
Luis <UO257809@uniovi.es>
2019-03-05 01:12:36 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

--- sdi806-lab-spring/src/test/java/com/uniovi/tests/Sdi806_lab_springTests.java
+++ sdi806-lab-spring/src/test/java/com/uniovi/tests/Sdi806_lab_springTests.java
@@ -9,6 +9,10 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.uniovi.tests.pageobject.PO_HomeView;
+import com.uniovi.tests.pageobject.PO_NavView;
+import com.uniovi.tests.pageobject.PO_Properties;
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
@@ -52,11 +56,27 @@ public class Sdi806_lab_springTests {
 		// Cerramos el navegador al finalizar las pruebas
 		driver.quit();
 	}
+	//PR01. Acceder a la página principal /
 	@Test
-	public void test() {
-		driver.get("http://www.google.fr");
-		driver.get("http://www.google.fr/search?q=cake");
-		
+	public void PR01() {
+	PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
 	
+	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+	PO_NavView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home
+	@Test
+	public void PR03() {
+		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español
+	@Test
+	public void PR04() {
+	PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish",
+	PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+	//SeleniumUtils.esperarSegundos(driver, 2);
+	}
 }
